355b37c801d63d52bd039adb14035efc0432d645
Sarah Melancon <sarahmelancon@Sarahs-MacBook-Pro.local>
2019-04-06 15:41:47 -0700

    basic login system

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 import os
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # Initialization
 # Create an application instance (an object of class Flask)  which handles all requests.
 application = Flask(__name__)
 application.secret_key = os.urandom(24)
+application.config.from_object(Config)
+db = SQLAlchemy(application)
+db.create_all()
+db.session.commit()
+
 Bootstrap(application)
 
-from app import routes  # Added at the bottom to avoid circular dependencies. (Altough it violates PEP8 standards)
+# login_manager needs to be initiated before running the app
+login_manager = LoginManager()
+login_manager.init_app(application)
 
+from app import classes
+from app import routes  # Added at the bottom to avoid circular dependencies. (Altough it violates PEP8 standards)
--- code/app/routes.py
+++ code/app/routes.py
@@ -1,7 +1,62 @@
-from app import application
-from flask import render_template
-
+from app import application, classes, db
+from flask import render_template, redirect, url_for
+from flask_login import current_user, login_user, login_required, logout_user
 
 @application.route('/')
 def index():
     return render_template('index.html')
+
+@application.route('/register')
+def register():
+    username = 'diane'
+    password = 'pwd'
+    email='diane@gmail.com'
+
+    user_count = classes.User.query.filter_by(username=username).count() \
+                    + classes.User.query.filter_by(email=email).count()
+
+    if user_count > 0:
+        return '<h1> Error - Existing user : ' + username + ' or ' + email + '</h1>'
+
+    else:
+        user = classes.User(username, email, password)
+        db.session.add(user)
+        db.session.commit()
+        return '<h1> Registered : ' + username + '</h1>'
+
+
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    username = 'diane'
+    password = 'pwd'
+    # Look for it in the database.
+    user = classes.User.query.filter_by(username=username).first()
+
+    # Login and validate the user.
+    if user is not None and user.check_password(password):
+        login_user(user)
+        return redirect(url_for('secret_page'))
+
+    else:
+        return '<h1> Invalid username and password combination! </h1>'
+
+
+@application.route('/logout')
+@login_required
+def logout():
+    before_logout = '<h1> Before logout - is_autheticated : ' \
+                    + str(current_user.is_authenticated) + '</h1>'
+
+    logout_user()
+
+    after_logout = '<h1> After logout - is_autheticated : ' \
+                   + str(current_user.is_authenticated) + '</h1>'
+    return before_logout + after_logout
+
+
+@application.route('/secret_page')
+@login_required
+def secret_page():
+    return render_template('secret.html', name=current_user.username, email=current_user.email)
+
+
