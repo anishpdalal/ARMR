8dc3b784957f2c37ecbeeac183753d3e6f55da97
Sarah Melancon <sarahmelancon@Sarahs-MacBook-Pro.local>
2019-04-06 17:15:44 -0700

    adding login template

--- code/app/routes.py
+++ code/app/routes.py
@@ -1,7 +1,7 @@
 from app import application, classes, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask_login import current_user, login_user, login_required, logout_user
-from classes import LogInForm, RegistrationForm
+from classes import LogInForm, RegistrationForm, User
 
 @application.route('/')
 def index():
@@ -14,71 +14,55 @@ def register():
     if registration_form.validate_on_submit():
         username = registration_form.username.data
         password = registration_form.password.data
-        # password_confirmation = registration_form.password_confirmation.data
+        password_confirmation = registration_form.password_confirmation.data
         # verification_code_input = registration_form.access_code.data
         
-        # verification = Verification.query.first() 
+        verification = Verification.query.first() 
 
-        # user_count = User.query.filter_by(username=username).count()
+        user_count = User.query.filter_by(username=username).count()
 
-        # error_count = 0
-        # if(password != password_confirmation):
-        #     error_count = error_count + 1 
-        #     flash('Error - Password Mismatch<br/>' )
+        error_count = 0
+        if(password != password_confirmation):
+            error_count = error_count + 1 
+            flash('Error - Password Mismatch<br/>' )
         
         # if(not verification.check_code(verification_code_input)):
         #     error_count = error_count + 1 
         #     flash('Error - Invalid Access Code<br/>' )
 
-        # if(not (verification.check_pwd_digit(password) and verification.check_pwd_upper(password) and verification.check_pwd_length(password))):
-        #     error_count = error_count + 1 
-        #     flash('Error - Password should be at least 8 digits with at least one number and one uppercase<br/>' )
+        if(not (verification.check_pwd_digit(password) and verification.check_pwd_upper(password) and verification.check_pwd_length(password))):
+            error_count = error_count + 1 
+            flash('Error - Password should be at least 8 digits with at least one number and one uppercase<br/>' )
         
-        # if(user_count > 0):
-        #     error_count = error_count + 1 
-        #     flash('Error - Existing user : ' + username + '<br/>')
+        if(user_count > 0):
+            error_count = error_count + 1 
+            flash('Error - Existing user : ' + username + '<br/>')
             
-        # if(error_count == 0):
-        #     user = User(username, password)
-        #     db.session.add(user)
-        #     db.session.commit()
-        #     return redirect(url_for('login'))
-            
-	return render_template('register.html', form=registration_form)
+        if(error_count == 0):
+            user = User(username, password)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login'))
+    return render_template('register.html', form=registration_form)
 
 
 @application.route('/login', methods=['GET', 'POST'])
 def login():
-    username = 'diane'
-    password = 'pwd'
-    # Look for it in the database.
-    user = classes.User.query.filter_by(username=username).first()
+    login_form = LogInForm()
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        password = login_form.password.data
+        # Look for it in the database.
+        user = User.query.filter_by(username=username).first()
 
-    # Login and validate the user.
-    if user is not None and user.check_password(password):
-        login_user(user)
-        return redirect(url_for('secret_page'))
+        # Login and validate the user.
+        if user is not None and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('alert'))
+        else:
+            flash('Invalid username and password combination!')
 
-    else:
-        return '<h1> Invalid username and password combination! </h1>'
-
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     login_form = LogInForm()
-#     if login_form.validate_on_submit():
-#         username = login_form.username.data
-#         password = login_form.password.data
-#         # Look for it in the database.
-#         user = User.query.filter_by(username=username).first()
-
-#         # Login and validate the user.
-#         if user is not None and user.check_password(password):
-#             login_user(user)
-#             return redirect(url_for('alert'))
-#         else:
-#             flash('Invalid username and password combination!')
-# 	return render_template('login.html', form=login_form)
+    return render_template('login.html', form=login_form)
 
 
 @application.route('/logout')
@@ -94,16 +78,8 @@ def logout():
     return before_logout + after_logout
 
 
-# @app.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-# return redirect(url_for('index'))
-
-
 @application.route('/secret_page')
 @login_required
 def secret_page():
     return render_template('secret.html', name=current_user.username, email=current_user.email)
-
 
