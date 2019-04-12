147f3571bb3522fdef1514ed5de8b44ef02b598a
Anish Dalal <anishdalal1@gmail.com>
2019-04-12 01:15:48 -0700

    emergency bug fixes

--- code/config.py
+++ code/config.py
@@ -3,5 +3,5 @@ class Config(object):
     user = "armrMaster"  # replace with server username
     pw = "armr_pw603"  # replace with server password
     SQLALCHEMY_DATABASE_URI = f"postgresql://{user}:\
-        {pw}@armr.c4eooxhj8ss8.us-west-1.rds.amazonaws.com:5432/armr"
+{pw}@armr.c4eooxhj8ss8.us-west-1.rds.amazonaws.com:5432/armr"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
